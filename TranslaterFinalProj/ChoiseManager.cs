da89787d6a0ead9b68b2f6cd39d406a4c4d3b859
Chulkov Arkadii <arkadijculkov2@gmail.com>
2025-04-17 16:56:03 +0300

    Made it

--- TranslaterFinalProj/ChoiseManager.cs
+++ TranslaterFinalProj/ChoiseManager.cs
@@ -82,7 +82,7 @@ namespace TranslaterFinalProj
                     case 3:
                         string[] translatedWords2;
                         string word2;
-                        Console.WriteLine("Choose the language to translate to:");
+                        Console.WriteLine("Choose the language to translate from:");
                         string Lan = Console.ReadLine();
                         AllDickt.TryGetValue(Lan, out CurrentDickt);
                         Translate = new Translater(CurrentDickt);
@@ -135,7 +135,7 @@ namespace TranslaterFinalProj
                             string[] str = new string[10];
                             for (var i = 0; i > -1; i++) 
                             {
-                                Console.WriteLine("Enter Translate");
+                                Console.WriteLine("Enter Translate or exit");
                                 str[i] = Console.ReadLine();
                                 if (str[i] == "exit")
                                 {
@@ -187,6 +187,10 @@ namespace TranslaterFinalProj
                             string word1 = Console.ReadLine().ToLower();
                             if (AllDickt[lang1].ContainsKey(word1))
                             {
+                                foreach (var it in AllDickt[lang1][word1])
+                                {
+                                    Console.WriteLine($"- {it}");
+                                }
                                 string[] translations = new string[AllDickt[lang1][word1].Length];
                                 for (int i = 0; i < AllDickt[lang1][word1].Length; i++)
                                 {
@@ -210,133 +214,162 @@ namespace TranslaterFinalProj
                         break;
                     // Delete word or translate
                     case 3:
-                        Console.WriteLine("Do you want to delete word or translate");
-                        string word2 = Console.ReadLine();
-                        Console.WriteLine("Enter the Language");
+                        Console.WriteLine("word or translate");
+                        string action = Console.ReadLine().ToLower();
+
+                        Console.WriteLine("Language:");
                         string lang2 = Console.ReadLine();
-                        ShowWordsToTranslate(lang2);
-                        if (word2 == "word")
+
+                        if (!AllDickt.ContainsKey(lang2))
                         {
-                            Console.WriteLine("Enter the word");
-                            string word1 = Console.ReadLine();
-                            if (AllDickt[lang2].ContainsKey(word1))
+                            Console.WriteLine("No language.");
+                            break;
+                        }
+
+                        Console.WriteLine("Word:");
+                        string word2 = Console.ReadLine().ToLower();
+
+                        if (!AllDickt[lang2].ContainsKey(word2))
+                        {
+                            Console.WriteLine("Word not found.");
+                            break;
+                        }
+
+                        if (action == "word")
+                        {
+                            AllDickt[lang2].Remove(word2);
+                            Console.WriteLine("Deleted");
+                        }
+                        else if (action == "translate")
+                        {
+                            string[] translations = AllDickt[lang2][word2];
+
+                            if (translations.Length == 1)
                             {
-                                AllDickt[lang2].Remove(word1);
-                                FileManage.SaveDictionaries(AllDickt);
+                                Console.WriteLine("Its Last translatoin so you cant delete it");
+                            }
+                            else
+                            {
+                                Console.WriteLine("translations: ");
+                                foreach (var t in translations)
+                                    Console.WriteLine($"- {t}");
+
+                                Console.WriteLine("Enter translation to delete: ");
+                                string toDelete = Console.ReadLine();
+
+                                string[] updated = translations.Where(t => t != toDelete).ToArray();
+
+                                if (updated.Length == translations.Length)
+                                {
+                                    Console.WriteLine("Translation not found");
+                                }
+                                else
+                                {
+                                    AllDickt[lang2][word2] = updated;
+                                    Console.WriteLine("Deleted");
+                                }
                             }
                         }
-                        else if (word2 == "translate")
+                        else
                         {
-                            Console.WriteLine("Enter the word");
-                            string word1 = Console.ReadLine();
-                            Console.WriteLine("some - 1 translation or all - 2");
-                            string choice = Console.ReadLine();
-                            if (AllDickt[lang2].ContainsKey(word1))
-                            {
-                                string[] temp = AllDickt[lang2][word2];
-                                if (temp.Length == 1) 
-                                {
-                                    AllDickt[lang2].Remove(word1);
-                                    FileManage.SaveDictionaries(AllDickt);
-                                }
-                                else if (choice == "1")
-                                {
-                                    Console.WriteLine("Enter the number of translation");
-                                    int num = Convert.ToInt32(Console.ReadLine());
-                                    string[] newTemp = new string[temp.Length - 1];
-                                    for (int i = 0; i < temp.Length; i++)
-                                    {
-                                        if (i != num)
-                                        {
-                                            newTemp[i] = AllDickt[lang2][word1][i];
-                                        }
-                                    }
-                                    AllDickt[lang2].Remove(word1);
-                                    AllDickt[lang2].Add(word1, newTemp);
-                                }
-                                else if (choice == "2")
-                                {
-                                    string[] newTemp = new string[temp.Length - 1];
-                                    for (int i = 0; i < temp.Length; i++)
-                                    {
-                                        newTemp[i] = temp[i];
-                                    }
-                                    AllDickt[lang2].Remove(word1);
-                                    AllDickt[lang2].Add(word1, newTemp);
-                                }
-                                FileManage.SaveDictionaries(AllDickt);
-                            }
+                            Console.WriteLine("Invalid choice");
                         }
+
+                        FileManage.SaveDictionaries(AllDickt);
                         break;
                     // Add word or translate
                     case 4:
-                        Dictionary<string, string[]> newDickt1 = new Dictionary<string, string[]>();
-                        Console.WriteLine("Enter the language");
+                        Console.WriteLine("Enter Language:");
                         string lang3 = Console.ReadLine();
-                        Console.WriteLine("Add word or translate");
-                        string cho = Console.ReadLine();
-                        if (cho == "word")
+
+                        if (!AllDickt.ContainsKey(lang3))
                         {
-                            while(true)
-{
-                                Console.WriteLine("Enter the word or exit");
-                                string worde = Console.ReadLine().ToLower();
-
-                                if (worde == "exit")
-                                {
-                                    break;
-                                }
-                                string[] str = new string[10];
-                                for (var i = 0; i > 10; i++)
-                                {
-                                    Console.WriteLine("Enter Translate");
-                                    str[i] = Console.ReadLine();
-                                    if (str[i] == "exit")
-                                    {
-                                        str[i] = null;
-                                        break;
-                                    }
-                                }
-
-                                string[] strings = str.Where(s => s != null).ToArray();
-                                newDickt1.Add(worde, strings);
-                                break;
-                            }
+                            AllDickt[lang3] = new Dictionary<string, string[]>();
                         }
-                        else if (cho == "translate")
+
+                        Console.WriteLine("word or translate");
+                        string choice = Console.ReadLine().ToLower();
+
+                        if (choice == "word")
+                        {
+                            while (true)
+                            {
+                                Console.WriteLine("word or exit:");
+                                string word4 = Console.ReadLine().ToLower();
+                                if (AllDickt[lang3].ContainsKey(word4))
+                                {
+                                    Console.WriteLine("word already exists");
+                                    continue;
+                                }
+
+                                if (word4 == "exit") break;
+
+                                List<string> translations = new List<string>();
+                                while (true)
+                                {
+                                    Console.WriteLine("Translate or exit:");
+                                    string translate = Console.ReadLine();
+
+                                    if (translate == "exit")
+                                        break;
+                                    
+                                    translations.Add(translate);
+                                }
+                                AllDickt[lang3].Add(word4, translations.ToArray());
+                                Console.WriteLine("Sdded");
+                            }
+
+                            FileManage.SaveDictionaries(AllDickt);
+                        }
+                        else if (choice == "translate")
                         {
                             ShowWordsToTranslate(lang3);
-                            Console.WriteLine("Enter the word");
-                            string word1 = Console.ReadLine().ToLower();
-                            if (AllDickt[lang3].ContainsKey(word1))
+
+                            Console.WriteLine("word:");
+                            string word4 = Console.ReadLine().ToLower();
+
+                            if (AllDickt[lang3].ContainsKey(word4))
                             {
-                                Console.WriteLine("Enter the number of translation");
-                                string[] translations = new string[Convert.ToInt32(Console.ReadLine())];
-                                for (int i = 0; i < AllDickt[lang3][word1].Length; i++)
+                                Console.WriteLine("translatoin count");
+                                if (int.TryParse(Console.ReadLine(), out int count) && count > 0)
                                 {
-                                    Console.WriteLine("Enter the new translate");
-                                    string newTranslate = Console.ReadLine();
-                                    translations[i] = newTranslate;
-                                }
-                                string[] res = new string[AllDickt[lang3][word1].Length + translations.Length];
+                                    List<string> newTranslations = new List<string>();
+                                    for (int i = 0; i < count; i++)
+                                    {
+                                        Console.WriteLine("translation: ");
+                                        newTranslations.Add(Console.ReadLine());
+                                    }
 
-                                for (int i = 0; i < AllDickt[lang3][word1].Length; i++)
-                                {
-                                    res[i] = AllDickt[lang3][word1][i];
-                                }
-                                for (int i = 0; i < translations.Length; i++)
-                                {
-                                    res[i + AllDickt[lang3][word1].Length] = translations[i];
-                                }
+                                    string[] existing = AllDickt[lang3][word4];
+                                    int l = existing.Length + newTranslations.ToArray().Length;
+                                    string[] newArr = new string[l];
+                                    existing.CopyTo(newArr, 0);
+                                    newTranslations.ToArray().CopyTo(newArr, existing.Length);
+                                    AllDickt[lang3].Remove(word4);
+                                    AllDickt[lang3].Add(word4, newArr);
 
-                                AllDickt[lang3][word1] = res;
-                                FileManage.SaveDictionaries(AllDickt);
+                                    FileManage.SaveDictionaries(AllDickt);
+                                    Console.WriteLine("Added");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("incorect num");
+                                }
                             }
+                            else
+                            {
+                                Console.WriteLine("word not found");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("wrong choise.");
                         }
                         break;
                     // Back to main menu
                     case 5:
                         break;
+                    // Invalid
                     default:
                         Console.WriteLine("Invalid input. Please enter a number.");
                         break;
